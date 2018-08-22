53e641f01042113d85db3554baeb8c77ddb9edb8
Ivan Dachev <i_dachev@yahoo.co.uk>
2018-08-22 07:23:31 +0300

    Updated

--- apt_install_no_gui.sh
+++ apt_install_no_gui.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-sudo apt-get install -y python-software-properties debconf-utils
+sudo -H apt-get -y install python-software-properties debconf-utils apt-transport-https \
+ ca-certificates curl software-properties-common
+
+################################################################################
+# Setup for latest Docker CE
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -H apt-key add -
+
+sudo -H apt-key fingerprint 0EBFCD88
+
+sudo -H add-apt-repository \
+ "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+################################################################################
+# Setup for Java and others
 
 sudo -H add-apt-repository -y ppa:ricotz/experimental
 sudo -H add-apt-repository -y ppa:webupd8team/java
@@ -9,7 +23,8 @@ sudo -H dpkg --add-architecture i386
 
 sudo -H apt-get update
 
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" \
+ | sudo -H debconf-set-selections
 
 sudo -H apt-get -y install htop ncdu vim tmux zsh git gitk zip aspell aptitude \
  keychain gparted smartmontools build-essential nvme-cli oracle-java8-installer \
@@ -17,13 +32,10 @@ sudo -H apt-get -y install htop ncdu vim tmux zsh git gitk zip aspell aptitude \
  pcsc-tools pcscd opensc libnss3-tools sshpass nmap python-pyqtgraph socat \
  pyqt4-dev-tools pdftk lrzip p7zip p7zip-full libimage-exiftool-perl \
  ffmpeg postgresql-client python-dev fdupes fslint gthumb mc archivemount \
- openssh-server maven libcurl4-openssl-dev mongodb-clients gcc g++ make
+ openssh-server maven libcurl4-openssl-dev mongodb-clients gcc g++ make 
 
 # ia32-libs is only available in mint - separate in new line to not fail above in Ubuntu
 sudo -H apt-get -y install ia32-libs
-
-sudo groupadd docker
-sudo gpasswd -a $USER docker
 
 sudo -H pip install -U pip 
 sudo -H pip install setuptools
@@ -34,7 +46,21 @@ sudo -H pip install bcrypt
 sudo -H pip install RBTools
 sudo -H pip install natsort
 
+################################################################################
+# Setup for nodejs and angular/cli
+
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo -H apt-get -y install nodejs
 
 sudo -H npm install -g @angular/cli
+
+sudo -H apt-get -y install docker-ce
+
+################################################################################
+# Install docker
+#
+# To install specific version check
+# sudo -H apt-cache madison docker-ce
+
+sudo groupadd docker
+sudo gpasswd -a $USER docker
