3787f6b64665a27b98e083ec27a88aa769548ab8
Ivan Dachev <i_dachev@yahoo.co.uk>
2018-08-22 08:15:21 +0300

    Updated

--- apt_install_no_gui.sh
+++ apt_install_no_gui.sh
@@ -14,6 +14,24 @@ sudo -H add-apt-repository \
  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 ################################################################################
+# Setup for latest Mongo 4.0
+
+# sudo -H apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+# --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.0 multiverse" \
+# | sudo -H tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+################################################################################
+# Setup for latest Mongo 3.6
+
+sudo -H apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+ --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/3.6 multiverse" \
+ | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+
+################################################################################
 # Setup for Java and others
 
 sudo -H add-apt-repository -y ppa:ricotz/experimental
@@ -32,7 +50,10 @@ sudo -H apt-get -y install htop ncdu vim tmux zsh git gitk zip aspell aptitude \
  pcsc-tools pcscd opensc libnss3-tools sshpass nmap python-pyqtgraph socat \
  pyqt4-dev-tools pdftk lrzip p7zip p7zip-full libimage-exiftool-perl \
  ffmpeg postgresql-client python-dev fdupes fslint gthumb mc archivemount \
- openssh-server maven libcurl4-openssl-dev mongodb-clients gcc g++ make 
+ openssh-server maven libcurl4-openssl-dev gcc g++ make
+
+# Mongo shell and tools 3.6/4.0
+sudo -H apt-get -y install mongodb-org-shell mongodb-org-tools
 
 # ia32-libs is only available in mint - separate in new line to not fail above in Ubuntu
 sudo -H apt-get -y install ia32-libs
