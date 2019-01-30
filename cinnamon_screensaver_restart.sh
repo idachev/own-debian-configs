d76afe14fab39bce86e39c90a1fe5fc2d697a457
Ivan Dachev <i_dachev@yahoo.co.uk>
2019-01-30 10:40:31 +0200

    Updated

--- cinnamon_screensaver_restart.sh
+++ cinnamon_screensaver_restart.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+WAIT_SECONDS=3
+
 sudo killall cinnamon-screensaver
+
+echo -e "\nWait ${WAIT_SECONDS}s the kill..."
+sleep ${WAIT_SECONDS}
+
+echo -e "\nReturn back to GUI with Ctrl + F7 or F8"
+
+echo -e "\nExecute in a GUI terminal:"
+echo "cinnamon-screensaver > /dev/null 2>&1 &"
 
