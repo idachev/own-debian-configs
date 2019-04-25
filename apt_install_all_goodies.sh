e9bd7993e676eb4157abe1ff4a3092002c3282d4
Ivan Dachev <i_dachev@yahoo.co.uk>
2019-04-25 11:11:36 +0300

    Fix gui updates

--- apt_install_all_goodies.sh
+++ apt_install_all_goodies.sh
@@ -3,6 +3,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ${DIR}/apt_install_no_gui.sh
 
-sudo -H apt-get -y install ttf-dejavu-core glogg xbacklight sublime-text-installer kate \
- handbrake psensor google-chrome-beta parcellite slack-desktop gwenview kdiff3 \
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | \
+  sudo apt-key add -
+
+sudo rm /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | \
+  sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | \
+  sudo apt-key add -
+
+rm /etc/apt/sources.list.d/google-chrome.list
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | \
+  sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+sudo apt-get update
+
+sudo -H apt-get -y install sublime-text-installer
+sudo -H apt-get -y install google-chrome-beta
+sudo -H apt-get -y install slack-desktop
+
+sudo -H apt-get -y install ttf-dejavu-core glogg xbacklight kate \
+ handbrake psensor parcellite gwenview kdiff3 \
  shutter pgadmin3 libreoffice gpick spotify-client font-manager
