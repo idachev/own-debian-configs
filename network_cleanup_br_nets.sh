d99277387f4c173a691dabd8b4e97bb80830ad82
Ivan Dachev <i_dachev@yahoo.co.uk>
2019-12-11 14:49:17 +0200

    Fixes

--- network_cleanup_br_nets.sh
+++ network_cleanup_br_nets.sh
@@ -3,11 +3,8 @@
 echo -e "\nBefore"
 sudo route -n
 
-sudo ifconfig br-9e937bbda099 down
-sudo ifconfig br-4103498a9c96 down
-sudo ifconfig br-6cc51567be89 down
-sudo ifconfig br-e2b737621188 down
-sudo ifconfig br-d0fdb5c8a2a8 down
+route -n | awk -F ' ' '{print $8}' | grep 'br-' | \
+  xargs -I{} sudo ifconfig "{}" down
 
 echo -e "\nAfter"
 sudo route -n
