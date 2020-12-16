8916d2413da8ea29043555f82765686b5a1b7093
Ivan Dachev <i_dachev@yahoo.co.uk>
2020-12-16 07:49:54 +0200

    Updated

---
+++ _transform_bionic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# To easy execute this do:
+# wget https://github.com/idachev/own-debian-configs/raw/master/_transform_bionic.sh; chmod +x ./_transform_bionic.sh; . ./_transform_bionic.sh; rm ./_transform_bionic.sh; cd ~/
+
+TARGET_DIR=~/bin
+
+if ! mkdir "${TARGET_DIR}"; then
+  echo -e "\nCreate ${TARGET_DIR} failed"
+  exit -1
+fi
+
+cd ${TARGET_DIR}
+
+sudo apt install -y git
+
+git clone https://github.com/idachev/own-debian-configs.git .
+
+find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i "s/\/home\/idachev/${HOME}/g"
+
+./apt_install_no_gui_bionic.sh
+
+cd ${TARGET_DIR}/settings/linux/home
+. create_links
+
+cd ${TARGET_DIR}
+
+echo -e "\nTrasnform completed please relogin"
+echo -e "\n\nFor desktop mint/ubuntu with GUI check ~/bin/apt_install_all_goodies_bionic.sh\n"
+
--- _transform_xenial.sh
+++ _transform_xenial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # To easy execute this do:
-# wget https://github.com/idachev/own-debian-configs/raw/master/_transform_.sh; chmod +x ./_transform_.sh; . ./_transform_.sh; rm ./_transform_.sh; cd ~/; bash
+# wget https://github.com/idachev/own-debian-configs/raw/master/_transform_xenial.sh; chmod +x ./_transform_xenial.sh; . ./_transform_xenial.sh; rm ./_transform_xenial.sh; cd ~/
 
 TARGET_DIR=~/bin
 
@@ -16,7 +16,7 @@ git clone https://github.com/idachev/own-debian-configs.git .
 
 find . -type f -not -path "./.git/*" -print0 | xargs -0 sed -i "s/\/home\/idachev/${HOME}/g"
 
-./apt_install_no_gui.sh
+./apt_install_no_gui_xenial.sh
 
 cd ${TARGET_DIR}/settings/linux/home
 . create_links
@@ -24,5 +24,5 @@ cd ${TARGET_DIR}/settings/linux/home
 cd ${TARGET_DIR}
 
 echo -e "\nTrasnform completed please relogin"
-echo -e "\n\nFor desktop mint/ubuntu with GUI check ~/bin/apt_install_all_goodies.sh\n"
+echo -e "\n\nFor desktop mint/ubuntu with GUI check ~/bin/apt_install_all_goodies_xenial.sh\n"
 
