cab522fd0cf1cbcfa6e31049851b88fcddeac505
Ivan Dachev <i_dachev@yahoo.co.uk>
2022-02-14 07:42:54 +0200

    Updated

--- docker_new_version_monitor.sh
+++ docker_new_version_monitor.sh
@@ -9,10 +9,13 @@ MONITOR_SHA=$3
 docker pull "${MONITOR_IMAGE}"
 CURRENT_SHA=$(docker inspect --format='{{index .RepoDigests 0}}' "${MONITOR_IMAGE}")
 
-echo "Found CURRENT_SHA=${CURRENT_SHA}"
+echo "Found CURRENT_SHA=${CURRENT_SHA}1"
 
 if [[ "${CURRENT_SHA}" != "${MONITOR_SHA}" ]]; then
-    FOUND_NEW_TAGS=$("${DIR}/docker_image_find_tag.sh" -n "${MONITOR_IMAGE_NAME}" -i "${MONITOR_IMAGE}")
+    FOUND_NEW_TAGS="Found new SHA for ${MONITOR_IMAGE}\nold: ${MONITOR_SHA}\nnew: ${CURRENT_SHA}\n\
+\nTo check for new tag execute:\ndocker_image_find_tag.sh -n ${MONITOR_IMAGE_NAME} -i ${MONITOR_IMAGE}\n\
+\nExecute this to update with new version, with proper major minor tag:\
+\n~/develop/personal/maven-3-openjdk-11-docker-client/build-and-push.sh 8 4"
 
     zenity --width 300 --warning --title 'New Version' --text "<span font-size=\"xx-large\">${FOUND_NEW_TAGS}</span>"
 fi
