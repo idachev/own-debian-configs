d9e7aea9dd16b1dc45deca8608b34678eea6aa72
Ivan Dachev <i_dachev@yahoo.co.uk>
2024-01-21 05:46:34 +0200

    Updated

--- brightnessctl_up.sh
+++ brightnessctl_up.sh
@@ -7,9 +7,9 @@ maxBrightness=$(brightnessctl m)
 
 scale=1000
 if (( ${currentBrightness} < ${maxBrightness}/100 )); then
-  scale=10
-elif (( ${currentBrightness} < ${maxBrightness}/50 )); then
   scale=50
+elif (( ${currentBrightness} < ${maxBrightness}/50 )); then
+  scale=75
 elif (( ${currentBrightness} < ${maxBrightness}/20 )); then
   scale=100
 elif (( ${currentBrightness} < ${maxBrightness}/10 )); then
