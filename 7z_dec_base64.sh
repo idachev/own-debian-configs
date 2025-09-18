fa17fb1b226739f695c88322c3842ecc015c07a3
Ivan Dachev <i_dachev@yahoo.co.uk>
2025-09-18 04:55:07 +0300

    Updated

--- 7z_dec_base64.sh
+++ 7z_dec_base64.sh
@@ -6,7 +6,7 @@ TMP=$(mktemp "/tmp/7z_dec_base64.XXXXXXXXXX")
 rm $TMP
 base64 -d "$INPUT" > $TMP
 
-7z x $TMP
+7z x -bb1 $TMP
 
 rm $TMP
 
--- 7z_enc.sh
+++ 7z_enc.sh
@@ -2,4 +2,4 @@
 
 archive=$1
 shift
-7z a -bd -mhe=on -p"${PASSWORD_7Z}" -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -mmt=on "$archive" "$@"
+7z a -bd -bb1 -mhe=on -p"${PASSWORD_7Z}" -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -mmt=on "$archive" "$@"
