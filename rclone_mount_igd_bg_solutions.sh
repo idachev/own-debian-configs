08f8a61c68c1a85802bd315dbded8a85d937751b
Ivan Dachev <i_dachev@yahoo.co.uk>
2026-06-02 05:29:01 +0300

    Moved to repo

--- rclone_mount_igd_bg_solutions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-exec "${SCRIPT_DIR}/rclone_mount_gdrive.sh" --remote gdrive-igd-bg-solutions / /home/idachev/storage_b/gdrive-igd-bg-solutions
